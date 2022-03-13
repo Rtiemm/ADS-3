4957d07e525070e592a6faa8510f540e0db2d20e
Rtiemm <99262095+Rtiemm@users.noreply.github.com>
2022-03-13 18:54:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int leftwall = 0;
+  int rigthwall = size - 1;
+  int count = 0;
+  while (rigthwall >= leftwall) {
+    if (arr[(leftwall + rigthwall) / 2] == value) {
+      count++;
+      break;
+    }
+    if (arr[(leftwall + rigthwall) / 2] < value) {
+      leftwall = (leftwall + rigthwall) / 2 + 1;
+    }
+    if (arr[(leftwall + rigthwall) / 2] > value) {
+      rigthwall = (leftwall + rigthwall) / 2 - 1;
+    }
+  }
+  if (count != 0) {
+    for (int i = (leftwall + rigthwall) / 2 + 1; arr[i] == value && i <= size - 1; i++) {
+      count++;
+    }
+    for (int i = (leftwall + rigthwall) / 2 - 1; arr[i] == value && i >= 0; i--) {
+      count++;
+    }
+  }
+  return count;
 }
